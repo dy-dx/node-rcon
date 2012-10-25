02b37590201492a7796440941a80d7a7d78f5458
Christopher Kim <christopherjkim@gmail.com>
2012-10-25 17:38:29 -0400

    Fix missing semicolons & whitespace formatting.

--- examples/minecraft.js
+++ examples/minecraft.js
@@ -1,4 +1,3 @@
-
 var Rcon = require('../node-rcon').newHandle;
 var rcon = new Rcon();
 
@@ -6,20 +5,19 @@ rcon.connect("localhost", 25575, "supersecret", onConnected);
 
 
 function onConnected(err, response){
-	if(err){console.error(err);return;}
+  if(err){console.error(err);return;}
 
-	console.log("connected", response);
-	
-	//make superman a op
-	var rc = rcon.sendCommand("op superman", function(err, response){
-		console.log("result of op:", err, response);	
-	});
-	
-	//revoke his newly found status
-	rcon.sendCommand("deop superman", function(err, response){
-		console.log("result of deop:", err, response);
-	});
-	
-	rcon.end();
+  console.log("connected", response);
+  
+  //make superman a op
+  var rc = rcon.sendCommand("op superman", function(err, response){
+    console.log("result of op:", err, response);
+  });
+  
+  //revoke his newly found status
+  rcon.sendCommand("deop superman", function(err, response){
+    console.log("result of deop:", err, response);
+  });
+  
+  rcon.end();
 }
-
--- node-rcon.js
+++ node-rcon.js
@@ -11,7 +11,7 @@ this.reqID=1;
 this.authed = false;
 this.online = false;
 
-this.isOnline = function() {return this.online;}//return online status of connection
+this.isOnline = function() {return this.online;}; //return online status of connection
 
 //queue management
 this.queue = {}; //keeps track of commands sent
@@ -21,13 +21,13 @@ this.queue = {}; //keeps track of commands sent
 * @param {Function} callback
 */
 this._addQueue = function(id, callback){
-	if (typeof(callback)==='undefined')return;
-	var q = this.queue;
-	var t = setTimeout(function(){
-		delete q[id]; //drop the timed out request from the queue 
-		callback({error:'Timed out'});
-	},TIMEOUT);
-	this.queue[id]={callback:callback, timeout:t};
+  if (typeof(callback)==='undefined')return;
+  var q = this.queue;
+  var t = setTimeout(function(){
+    delete q[id]; //drop the timed out request from the queue
+    callback({error:'Timed out'});
+  },TIMEOUT);
+  this.queue[id]={callback:callback, timeout:t};
 };
 
 /**
@@ -43,73 +43,79 @@ this._processQueue = function(response){
     //callback
     q.callback(null, response);
     //delete from the queue
-    delete this.queue[response.reqID];	
+    delete this.queue[response.reqID];
   }
-}
+};
 
 
 //connect command
-this.connect = function(ip,port,password,callback){
-	if(typeof(callback)==='undefined'){callback = function(err, response){};}
-        this.socket = net.createConnection(port,ip);
-        this.socket.rcon = this;
-        this.socket.on('error',function(err){return callback(err);});
-        this.socket.on('close',function(){this.rcon.authed=false;this.rcon.online=false});
-        this.socket.on('connect',function(){
-          this.rcon.online=true;
-          this.write(this.rcon.makePacket(0,AUTH_TYPE,password));
-        });	
-        this.socket.on('data',function(data){
-          var responses = this.rcon.readPacket(data);
-	  for(var i=0; i<responses.length;i++){
-	    var response = responses[i]; 
-            if(response.type === AUTH_RES_TYPE){
-              if(response.reqID == -1){
-                this.destroy();
-                callback('AUTH FAILED');
-                return;	
-              }else{
-		this.rcon.authed=true;
-		callback(null, response); 
-		return;
-	      }
-            }
-	    this.rcon._processQueue(response);
-	  }
-        });
-      };
+this.connect = function(ip, port, password, callback){
+  if (typeof(callback)==='undefined') {callback = function(err, response){};}
+  this.socket = net.createConnection(port, ip);
+  this.socket.rcon = this;
+  this.socket.on('error', function(err) {
+    return callback(err);
+  })
+  .on('close', function() {
+    this.rcon.authed = false;
+    this.rcon.online = false;
+  })
+  .on('connect', function() {
+    this.rcon.online = true;
+    this.write(this.rcon.makePacket(0, AUTH_TYPE, password));
+  })
+  .on('data', function(data) {
+    var responses = this.rcon.readPacket(data);
+    for (var i=0; i<responses.length; i++) {
+      var response = responses[i];
+      if (response.type === AUTH_RES_TYPE) {
+        if (response.reqID == -1) {
+          this.destroy();
+          callback('AUTH FAILED');
+          return;
+        } else {
+          this.rcon.authed = true;
+          callback(null, response);
+          return;
+        }
+      }
+      this.rcon._processQueue(response);
+    }
+  });
+};
 
 //end connection
 this.end = function(){
-	this.socket.end();
-}
+  this.socket.end();
+};
 
 
-this.sendCommand = function(command,callback) {
-	var msg;
-	if (typeof(callback)==='undefined') {callback = function(err, response){};}
-        if(!this.online){msg={"error":'Not Online'};callback(msg);return msg;}
-        if(!this.authed){msg={"error":'Not Authenticated'};callback(msg); return msg;}
-        var id = this.reqID+0;
-	this.socket.write(this.makePacket(id,COMMAND_TYPE,command));
-        this._addQueue(id, callback);
-	
-	this.reqID +=1;
-        return {"reqID":id};
-       };
+this.sendCommand = function(command, callback) {
+  var msg;
+  if (typeof(callback)==='undefined') {callback = function(err, response){};}
+  if(!this.online){msg={"error":'Not Online'};callback(msg);return msg;}
+  if(!this.authed){msg={"error":'Not Authenticated'};callback(msg); return msg;}
+
+  var id = this.reqID+0;
+  this.socket.write(this.makePacket(id,COMMAND_TYPE,command));
+  this._addQueue(id, callback);
+  
+  this.reqID +=1;
+  return {"reqID":id};
+};
 
 
        
 //makes a packet for RCON
 this.makePacket = function(req,type,S1){
-        var b = new Buffer(14+S1.length);
-        b.writeInt32LE(10+S1.length,0);
-        b.writeInt32LE(req,4);
-        b.writeInt32LE(type,8);
-        b.write(S1,12);
-        b.writeInt16LE(0,(12+S1.length));
-        return b;      
-      }
+  var b = new Buffer(14+S1.length);
+  b.writeInt32LE(10+S1.length,0);
+  b.writeInt32LE(req,4);
+  b.writeInt32LE(type,8);
+  b.write(S1,12);
+  b.writeInt16LE(0,(12+S1.length));
+  return b;
+};
 
 //read a packet out into a JSON object
 this.readPacket = function(packet){
@@ -127,8 +133,8 @@ this.readPacket = function(packet){
     responses.push(res);
     //there are more to read, at least one additional response
     if(packet.length > size+4){
-	//position on the next one
-	packet = packet.slice(size+4);
+  //position on the next one
+  packet = packet.slice(size+4);
     }
     else{
       //no more responses
@@ -140,5 +146,3 @@ this.readPacket = function(packet){
   };
 return this;
 };
-
-
